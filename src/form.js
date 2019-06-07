e2e626938790de8abf5837b2793fb1d3cdcee31b
paul <yummta@gmail.com>
2019-06-07 00:33:15 -0500

    Refactor Steps form **Form** - Add required attr to input name - Reset input after submit form - Disabled autocomplete - Add method to onChange input name **App** - Send setState like prop

--- src/form.js
+++ src/form.js
@@ -1,14 +1,29 @@
 import React from "react";
 
 function Form({ label, placeholder, saveName }) {
+  const [currentValue, setCurrentValue] = React.useState("");
+
   function setName(event) {
     event.preventDefault();
-    saveName(event.target.elements.name.value);
+    saveName(currentValue);
+    setCurrentValue("");
   }
+
+  function onChangeInputName(event) {
+    setCurrentValue(event.currentTarget.value);
+  }
+
   return (
     <form onSubmit={setName}>
       <label>{label}</label>
-      <input name="name" placeholder={placeholder} />
+      <input
+        required="required"
+        autoComplete="off"
+        name="name"
+        placeholder={placeholder}
+        onChange={onChangeInputName}
+        value={currentValue}
+      />
       <button type="submit">Next</button>
     </form>
   );
