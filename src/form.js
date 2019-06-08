c7b818cb81f678149c3ec38c4a8d2471ddb5a381
christopher77 <christoph.roa@gmail.com>
2019-06-07 21:18:21 -0500

    Add css to Form

--- src/form.js
+++ src/form.js
@@ -1,4 +1,6 @@
+/** @jsx jsx */
 import React from "react";
+import { jsx } from "@emotion/core";
 
 function Form({ label, placeholder, saveName }) {
   const [currentValue, setCurrentValue] = React.useState("");
@@ -14,18 +16,76 @@ function Form({ label, placeholder, saveName }) {
   }
 
   return (
-    <form onSubmit={setName}>
-      <label>{label}</label>
-      <input
-        required="required"
-        autoComplete="off"
-        name="name"
-        placeholder={placeholder}
-        onChange={onChangeInputName}
-        value={currentValue}
-      />
-      <button type="submit">Next</button>
-    </form>
+    <div
+      css={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+      }}
+    >
+      <form
+        onSubmit={setName}
+        css={{
+          margin: "auto",
+          backgroundColor: "white",
+          borderRadius: ".5rem",
+          padding: "2rem 1rem",
+          fontSize: "25px",
+          width: "80%",
+          maxWidth: 360,
+          textAlign: "center"
+        }}
+      >
+        <label
+          css={{
+            margin: 0,
+            fontWeight: "500",
+            textAlign: "center",
+            justifyContent: "center"
+          }}
+        >
+          {label}
+        </label>
+        <input
+          css={{
+            border: "none",
+            padding: ".5rem 1rem",
+            fontSize: "1.2rem",
+            boxSizing: "border-box",
+            width: "100%",
+            outline: "none",
+            transition: "border-bottom-color 300ms ease",
+            ":focus": {
+              borderBottomColor: "#7E00F3"
+            }
+          }}
+          required="required"
+          autoComplete="off"
+          name="name"
+          placeholder={placeholder}
+          onChange={onChangeInputName}
+          value={currentValue}
+        />
+        <button
+          css={{
+            backgroundColor: "blue",
+            borderRadius: ".5rem",
+            color: "white",
+            width: "100%",
+            padding: ".75rem .5rem",
+            boxSizing: "border-box",
+            fontSize: ".9rem",
+            fontWeight: "500",
+            textTransform: "uppercase",
+            outline: "none"
+          }}
+          type="submit"
+        >
+          Next
+        </button>
+      </form>
+    </div>
   );
 }
 
