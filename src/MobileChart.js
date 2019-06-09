1a7eee8c57352380db7b95813e3da8e3bf8596bf
Frank Davis Condezo Fabian <davis.con.fab@gmail.com>
2019-06-08 23:24:41 -0500

    Feature/ui chart mobile (#35)
    * Add styles
    
    * Add style arrows

--- src/MobileChart.js
+++ src/MobileChart.js
@@ -2,58 +2,66 @@
 import React, { useState } from "react";
 import { jsx } from "@emotion/core";
 import ChevronRigth from "./icons/chevron-rigth";
+import NewEmployee from "./new-employee";
+
+const avatar = {
+  width: "40px",
+  height: "40px",
+  borderRadius: "50%"
+};
+
+const item = {
+  display: "flex",
+  alignItems: "center",
+  borderBottom: "2px solid #ececec",
+  color: "#000"
+};
+
+const title = {
+  color: "#979797",
+  margin: "0"
+};
+
+const wrapIcon = {
+  backgroundColor: "#d9e7fd",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  marginLeft: "auto",
+  borderRadius: "50%",
+  color: "#223fce",
+  width: "36px",
+  height: "36px"
+};
+
+const name = {
+  textTransform: "capitalize",
+  margin: "0",
+  fontSize: "18px"
+};
 
 function Avatar({ user, ...props }) {
-  const wrap = {
-    display: "flex",
-    alignItems: "center"
-  };
-
-  const avatar = {
-    width: "40px",
-    height: "40px",
-    borderRadius: "50%"
-  };
-
-  const icon = {
-    margin: "5px",
-    color: "#979797"
-  };
-
   return (
-    <span role="menuitem" css={wrap} {...props}>
+    <span
+      role="menuitem"
+      css={{
+        display: "flex",
+        alignItems: "center"
+      }}
+      {...props}
+    >
       <img src={user.photo} css={avatar} alt="123" />
-      <ChevronRigth css={icon} />
+      <ChevronRigth
+        css={{
+          margin: "5px",
+          color: "#979797"
+        }}
+      />
     </span>
   );
 }
 
 function Employee({ employee, onClick }) {
-  const item = {
-    display: "flex",
-    alignItems: "center",
-    padding: "16px",
-    borderBottom: "2px solid #ececec",
-    color: "#000"
-  };
-
-  const avatar = {
-    width: "40px",
-    height: "40px",
-    borderRadius: "50%"
-  };
-
-  const title = {
-    color: "#979797",
-    margin: "0"
-  };
-
-  const name = {
-    textTransform: "capitalize",
-    margin: "0",
-    fontSize: "18px"
-  };
-
   return (
     <li css={item}>
       <img src={employee.photo} css={avatar} alt="123" />
@@ -65,27 +73,14 @@ function Employee({ employee, onClick }) {
         <h4 css={name}>{employee.name}</h4>
         <div css={title}>{employee.title}</div>
       </div>
-      <div
-        css={{
-          backgroundColor: "#d9e7fd",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          marginLeft: "auto",
-          borderRadius: "50%",
-          color: "#223fce",
-          width: "36px",
-          height: "36px"
-        }}
-        onClick={onClick}
-      >
+      <div css={wrapIcon} onClick={onClick}>
         <ChevronRigth />
       </div>
     </li>
   );
 }
 
-function MobileChart({ getChildren, parent }) {
+function MobileChart({ getChildren, parent, handleCreateChild }) {
   const [employeActive, setEmployeActive] = React.useState(parent);
   const [list, setList] = React.useState([parent]);
 
@@ -126,9 +121,32 @@ function MobileChart({ getChildren, parent }) {
         padding: "16px"
       }}
     >
-      <h2>Chart organigram</h2>
-      <div css={card}>
-        <div css={header}>
+      <div
+        css={{
+          textAlign: "center",
+          margin: "1em"
+        }}
+      >
+        <h1
+          css={{
+            fontSize: "25px"
+          }}
+        >
+          Company Name
+        </h1>
+        <h4
+          css={{
+            opacity: "0.8"
+          }}
+        >
+          Organization chart
+        </h4>
+      </div>
+
+      <div css={{ ...card, marginBottom: "1.5em" }}>
+        <div
+          css={{ ...header, "span:last-child": { svg: { display: "none" } } }}
+        >
           {list.map(employee => (
             <Avatar
               user={employee}
@@ -137,6 +155,37 @@ function MobileChart({ getChildren, parent }) {
             />
           ))}
         </div>
+      </div>
+
+      <div css={card}>
+        <div
+          css={{
+            display: "flex",
+            alignItems: "center",
+            padding: "16px",
+            boxShadow: "2px 4px 24px 0 rgba(0, 0, 0, 0.15)"
+          }}
+        >
+          <img
+            src={employeActive.photo}
+            alt={employeActive.name}
+            css={avatar}
+          />
+          <div
+            css={{
+              margin: "0 8px",
+              lineHeight: "1.3"
+            }}
+          >
+            <h4 css={name}>{employeActive.name}</h4>
+            <div css={title}>{employeActive.title}</div>
+          </div>
+          <NewEmployee
+            employee={employeActive}
+            handleCreateChild={handleCreateChild}
+          />
+        </div>
+
         <ul css={items}>
           {getChildren(employeActive.id).map(employee => (
             <Employee
